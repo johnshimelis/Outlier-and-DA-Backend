57e2d91dc7f8551c35547eb3e30072e6d4d35081
John Shimelis <johnshimelis09@gmail.com>
2025-04-03 16:26:34 +0300

    Update adsRoutes.js

--- src/routes/adsRoutes.js
+++ src/routes/adsRoutes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const adsController = require("../controllers/adsController");
 
-// Routes
+// Upload ad (uses multer middleware from controller)
 router.post("/:type", adsController.uploadAd);
+
+// Get ads by type
 router.get("/:type", adsController.getAds);
+
+// Delete ad by ID
 router.delete("/:id", adsController.deleteAd);
+
+// Update ad by ID (uses multer middleware from controller)
 router.put("/:id", adsController.updateAd);
 
 module.exports = router;
