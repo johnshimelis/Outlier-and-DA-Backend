7b0cb5996fa8a52233b227d2f867a5c234629201
John Shimelis <johnshimelis09@gmail.com>
2025-04-03 16:12:31 +0300

    Update adsRoutes.js

--- src/routes/adsRoutes.js
+++ src/routes/adsRoutes.js
@@ -1,28 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const adsController = require("../controllers/adsController");
-const { validateAdType } = require("../middleware/validators");
 
-// API versioning prefix
-router.use("/api/v1/ads", router);
-
-// Ads routes with controller middleware
-router.post("/:type", 
-  validateAdType, 
-  adsController.uploadAd
-);
-
-router.get("/:type", 
-  validateAdType, 
-  adsController.getAds
-);
-
-router.delete("/:id", 
-  adsController.deleteAd
-);
-
-router.put("/:id", 
-  adsController.updateAd
-);
+// Routes
+router.post("/:type", adsController.uploadAd);
+router.get("/:type", adsController.getAds);
+router.delete("/:id", adsController.deleteAd);
+router.put("/:id", adsController.updateAd);
 
 module.exports = router;
