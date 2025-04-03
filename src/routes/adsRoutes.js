fa68645887d7d29804db85cf741250ee22661213
John Shimelis <johnshimelis09@gmail.com>
2025-04-03 16:04:53 +0300

    Update adsRoutes.js

--- src/routes/adsRoutes.js
+++ src/routes/adsRoutes.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const adsController = require("../controllers/adsController");
+const { validateAdType } = require("../middleware/validators");
 
-// Routes
-router.post("/:type", adsController.uploadAd); // Multer middleware is already included in the controller
-router.get("/:type", adsController.getAds);
-router.delete("/:id", adsController.deleteAd);
-router.put("/:id", adsController.updateAd); // Multer middleware is already included in the controller
+// API versioning prefix
+router.use("/api/v1/ads", router);
+
+// Ads routes with controller middleware
+router.post("/:type", 
+  validateAdType, 
+  adsController.uploadAd
+);
+
+router.get("/:type", 
+  validateAdType, 
+  adsController.getAds
+);
+
+router.delete("/:id", 
+  adsController.deleteAd
+);
+
+router.put("/:id", 
+  adsController.updateAd
+);
 
 module.exports = router;
